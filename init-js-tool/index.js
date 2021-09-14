a3311edf1def665945746016dabbf7961ed1464c
he wang <hecool108@gmail.com>
2021-09-14 16:00:18 +0800

    Update pool creation steps

--- init-js-tool/index.js
+++ init-js-tool/index.js
@@ -1,25 +1,31 @@
 require('dotenv').config();
-const { initCoinPrice, setOpenOracle,test } = require('./oracles').default;
-const { createFWP, createCRP, createYTP, swapYForX } = require('./pools');
+const { initCoinPrice, setOpenOracle } = require('./oracles').default;
+const { createFWP, createCRP, createYTP, borrow } = require('./pools');
 async function run() {
-  // const {usdc, btc} = await initCoinPrice()
-  // Need to call it one by one, or you'll receive 'ConflictingNonceInMempool' Error
-  // await setOpenOracle('WBTC','CoinGecko', btc);
-  // await setOpenOracle('USDA','CoinGecko', usdc);
+  // const { usdc, btc } = await initCoinPrice();
+  //Need to call it one by one, or you'll receive 'ConflictingNonceInMempool' Error
+  // await setOpenOracle('WBTC','nothing', btc);
+  // await setOpenOracle('USDA','nothing', usdc);
   // await createFWP()
-//   await createYTP(
-//     'yield-wbtc-59760',
-//     'token-wbtc',
-//     'ytp-yield-wbtc-59760-wbtc'
-//   );
-//   await createYTP(
-//     'yield-wbtc-79760',
-//     'token-wbtc',
-//     'ytp-yield-wbtc-79760-wbtc'
-//   );
-//   await createCRP('yield-wbtc-59760', 'key-wbtc-59760-usda');
-//   await createCRP('yield-wbtc-79760', 'key-wbtc-79760-usda');
-//   await swapYForX('yield-wbtc-59760', 'token-wbtc');
-//   await swapYForX('yield-wbtc-79760', 'token-wbtc');
+  // await createYTP(
+  //   'yield-wbtc-59760',
+  //   'token-wbtc',
+  //   'ytp-yield-wbtc-59760-wbtc',
+  //   'multisig-ytp-yield-wbtc-59760-wbtc'
+  // );
+
+  // await createYTP(
+  //   'yield-wbtc-79760',
+  //   'token-wbtc',
+  //   'ytp-yield-wbtc-79760-wbtc',
+  //   'multisig-ytp-yield-wbtc-79760-wbtc'
+  // );
+  // await createYTP('yield-usda-59760','token-wbtc', 'pool-token-usda-59760-usda');
+  // await createCRP(
+  //   'yield-wbtc-59760',
+  //   'key-wbtc-59760-usda',
+  //   'multisig-ytp-yield-wbtc-59760-wbtc'
+  // );
+  // await borrow('yield-wbtc-59760', 'token-wbtc', 2);
 }
 run();
--- init-js-tool/pools.js
+++ init-js-tool/pools.js
@@ -1,97 +1,130 @@
+const { getPK, network } = require('./wallet');
 const {
-    getPK, network
-  } = require('./wallet');
-const {
-    makeContractCall,
-    AnchorMode,
-    PostConditionMode,
-    uintCV,
-    contractPrincipalCV,
-    broadcastTransaction
-  } = require('@stacks/transactions');
+  makeContractCall,
+  AnchorMode,
+  PostConditionMode,
+  uintCV,
+  contractPrincipalCV,
+  broadcastTransaction,
+} = require('@stacks/transactions');
 
 const createFWP = async () => {
+  const privateKey = await getPK();
+  const txOptions = {
+    contractAddress: process.env.ACCOUNT_ADDRESS,
+    contractName: 'fixed-weight-pool',
+    functionName: 'create-pool',
+    functionArgs: [
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-wbtc'),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-usda'),
+      uintCV(5 * 1e7),
+      uintCV(5 * 1e7),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'fwp-wbtc-usda-50-50'),
+      contractPrincipalCV(
+        process.env.ACCOUNT_ADDRESS,
+        'multisig-fwp-wbtc-usda-50-50'
+      ),
+      uintCV(100 * 1e8),
+      uintCV(5000 * 100 * 1e8),
+    ],
+    senderKey: privateKey,
+    validateWithAbi: true,
+    network,
+    anchorMode: AnchorMode.Any,
+    postConditionMode: PostConditionMode.Allow,
+  };
+  try {
+    const transaction = await makeContractCall(txOptions);
+    const broadcastResponse = await broadcastTransaction(transaction, network);
+    console.log(broadcastResponse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const createCRP = async (yiedToken, keyToken, multisig) => {
+  const privateKey = await getPK();
+  const txOptions = {
+    contractAddress: process.env.ACCOUNT_ADDRESS,
+    contractName: 'collateral-rebalancing-pool',
+    functionName: 'create-pool',
+    functionArgs: [
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-wbtc'),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-usda'),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, keyToken),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, multisig),
+      uintCV(0.8 * 1e8),
+      uintCV(0.95 * 1e8),
+      uintCV(0.8 * 1e8),
+      uintCV(0),
+      uintCV(50000 * 1e8),
+    ],
+    senderKey: privateKey,
+    validateWithAbi: true,
+    network,
+    anchorMode: AnchorMode.Any,
+    postConditionMode: PostConditionMode.Allow,
+  };
+  try {
+    const transaction = await makeContractCall(txOptions);
+    const broadcastResponse = await broadcastTransaction(transaction, network);
+    console.log(broadcastResponse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const createYTP = async (yiedToken, token, poolToken, multisig) => {
+  const privateKey = await getPK();
+  const txOptions = {
+    contractAddress: process.env.ACCOUNT_ADDRESS,
+    contractName: 'yield-token-pool',
+    functionName: 'create-pool',
+    functionArgs: [
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, token),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, poolToken),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, multisig),
+      uintCV(10 * 1e8),
+      uintCV(10 * 1e8),
+    ],
+    senderKey: privateKey,
+    validateWithAbi: true,
+    network,
+    anchorMode: AnchorMode.Any,
+    postConditionMode: PostConditionMode.Allow,
+  };
+  try {
+    const transaction = await makeContractCall(txOptions);
+    const broadcastResponse = await broadcastTransaction(transaction, network);
+    console.log(broadcastResponse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const borrow = async (yiedToken, token, dy)=>{
     const privateKey = await getPK();
-    const txOptions = {
-        contractAddress: process.env.ACCOUNT_ADDRESS,
-        contractName: 'fixed-weight-pool',
-        functionName: 'create-pool',
-        functionArgs: [
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-wbtc'),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-usda'),
-            uintCV(5*1e7),
-            uintCV(5*1e7),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS,'fwp-wbtc-usda-50-50'),
-            uintCV(10000e8),
-            uintCV(500000000e8),
-        ],
-        senderKey: privateKey,
-        validateWithAbi: true,
-        network,
-        anchorMode: AnchorMode.Any,
-        postConditionMode: PostConditionMode.Allow,
-    };
-    try {
-        const transaction = await makeContractCall(txOptions);
-        const broadcastResponse = await broadcastTransaction(transaction, network);
-        console.log(broadcastResponse);
-    } catch (error) {
-        console.log(error);
-    }
-}
-const createCRP = async (yiedToken, keyToken) => {
-    const privateKey = await getPK();
-    const txOptions = {
-        contractAddress: process.env.ACCOUNT_ADDRESS,
-        contractName: 'collateral-rebalancing-pool',
-        functionName: 'create-pool',
-        functionArgs: [
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-wbtc'),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, 'token-usda'),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, keyToken),
-            uintCV(100000*1e8),
-        ],
-        senderKey: privateKey,
-        validateWithAbi: true,
-        network,
-        anchorMode: AnchorMode.Any,
-        postConditionMode: PostConditionMode.Allow,
-    };
-    try {
-        const transaction = await makeContractCall(txOptions);
-        const broadcastResponse = await broadcastTransaction(transaction, network);
-        console.log(broadcastResponse);
-    } catch (error) {
-        console.log(error);
-    }
-}
-const createYTP = async (yiedToken, token, poolToken) => {
-    const privateKey = await getPK();
-    const txOptions = {
-        contractAddress: process.env.ACCOUNT_ADDRESS,
-        contractName: 'yield-token-pool',
-        functionName: 'create-pool',
-        functionArgs: [
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, token),
-            contractPrincipalCV(process.env.ACCOUNT_ADDRESS, poolToken),
-            uintCV(100*1e8),
-            uintCV(100*1e8),
-        ],
-        senderKey: privateKey,
-        validateWithAbi: true,
-        network,
-        anchorMode: AnchorMode.Any,
-        postConditionMode: PostConditionMode.Allow,
-    };
-    try {
-        const transaction = await makeContractCall(txOptions);
-        const broadcastResponse = await broadcastTransaction(transaction, network);
-        console.log(broadcastResponse);
-    } catch (error) {
-        console.log(error);
-    }
+  const txOptions = {
+    contractAddress: process.env.ACCOUNT_ADDRESS,
+    contractName: 'yield-token-pool',
+    functionName: 'swap-y-for-x',
+    functionArgs: [
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, yiedToken),
+      contractPrincipalCV(process.env.ACCOUNT_ADDRESS, token),
+      uintCV(dy * 1e8),
+    ],
+    senderKey: privateKey,
+    validateWithAbi: true,
+    network,
+    anchorMode: AnchorMode.Any,
+    postConditionMode: PostConditionMode.Allow,
+  };
+  try {
+    const transaction = await makeContractCall(txOptions);
+    const broadcastResponse = await broadcastTransaction(transaction, network);
+    console.log(broadcastResponse);
+  } catch (error) {
+    console.log(error);
+  }
 }
 const swapYForX = async (yieldToken, token)=>{
     const privateKey = await getPK();
@@ -114,4 +147,4 @@ const swapYForX = async (yieldToken, token)=>{
 exports.createFWP = createFWP;
 exports.createCRP = createCRP;
 exports.createYTP = createYTP;
-exports.swapYForX = swapYForX;
+exports.borrow = borrow;
