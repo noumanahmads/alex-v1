5f668b6761029e0d69064eaa48c4cdfcc8a3e4a9
jbotwina <jimbotwina16@gmail.com>
2021-09-14 00:36:15 -0400

    alexnet deplot

--- clarity/deploy-util/scripts/get-nonce.py
+++ clarity/deploy-util/scripts/get-nonce.py
@@ -3,7 +3,8 @@ import subprocess
 import os
 import json
 
-res = subprocess.check_output(f"cat ../hex-files/get-nonce/get-nonce1.hex | xxd -p -r | curl -H \"Content-Type: application/octet-stream\" -X POST --data-binary @- https://stacks-node-api.regtest.stacks.co/v2/transactions", shell=True)
+res = subprocess.check_output(f"cat ../hex-files/get-nonce/get-nonce1.hex | xxd -p -r | curl -H \"Content-Type: application/octet-stream\" -X POST --data-binary @- https://regtest-2.alexgo.io/v2/transactions", shell=True)
+print(res)
 data = json.loads(res)
 expected_nonce = data['reason_data']['expected']
 print(f"Nonce is {expected_nonce}")
--- clarity/deploy-util/scripts/initialize-account.py
+++ clarity/deploy-util/scripts/initialize-account.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 
 #make keychain
-subprocess.check_output("stx make_keychain -t | jq . > deploy-keychain.json", shell=True)
+# subprocess.check_output("stx make_keychain -t | jq . > deploy-keychain.json", shell=True)
 
-#get stx needed for deployment
-faucet = subprocess.check_output("curl -s -X POST \"https://stacks-node-api.regtest.stacks.co/extended/v1/faucets/stx?address=$(cat ./deploy-keychain.json | jq -r .keyInfo.address)\" | jq .", shell=True)
+# #get stx needed for deployment
+# faucet = subprocess.check_output("curl -s -X POST \"https://regtest-2.alexgo.io/extended/v1/faucets/stx?address=$(cat ./deploy-keychain.json | jq -r .keyInfo.address)\" | jq .", shell=True)
 
 #make directories to store contracts and hex files.
 os.mkdir('../staged-contracts')
